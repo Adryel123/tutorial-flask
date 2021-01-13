131e92bca0f943cf485a1863802ea847dcf11698
Adryel <adryel.pnp@gmail.com>
2021-01-13 00:24:41 -0300

    First commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/formulario', methods=['GET'])
+def get_form():
+    return render_template("formulario.html")
+
+
+@app.route('/trata-dados', methods=['POST'])
+def validate_form():
+    dados = request.form
+    texto = f"seu nome é {dados.get('name')}, seu endereço é {dados.get('address')} e você tem {dados.get('age')} anos :)"
+    return texto
+
+
+app.run()
