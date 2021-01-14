56c9bcef489b7ca24c01c26dfa0f2d2007318225
Adryel <adryel.pnp@gmail.com>
2021-01-14 12:58:54 -0300

    example codebase for a bot with web forms

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Criando páginas web
 Esse repositório faz parte de um tutorial que pode ser encontrado [aqui](https://www.notion.so/Servindo-p-ginas-web-com-o-flask-941af6ba6ca14458b6e15b67bbc9401d).
 
+## Exemplo de bot
+Nessa branch há uma ideia de bot que ao receber o comando /form, envia o link para um formulário. Em seguida, pega os dados recebidos no formulário e retorna ao usuário.
+
 ## Rodando o projeto
 * Clone esse repositório
 * Caso não tenha o Flask instalado, instale-o com o comando `pip install flask`
--- server.py
+++ server.py
@@ -1,18 +1,53 @@
 from flask import Flask, render_template, request
+import telepot
+import urllib3
+
+# ------------------------------------------- #
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(
+    proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+# ------------------------------------------- #
+
+TOKEN = 'seu token'
+usuario = 'seu usuario do PA'
+webhook = 'seu_segredo, pode ser o token do bot, por exemplo'
+
+bot = telepot.Bot(TOKEN)
+bot.setWebhook(
+    f"https://{usuario}.pythonanywhere.com/{webhook}", max_connections=1)
 
 app = Flask(__name__)
 
 
+@app.route(f'/{webhook}', methods=["post"])
+def trata_mensagem():
+    """rota para receber requisições webhook do telegram"""
+    mensagem = request.get_json()
+    texto = mensagem["message"]["text"]
+    id = mensagem["message"]["chat"]["id"]
+
+    if texto.startsWith("/form"):
+        bot.sendMessage(id, f"https://{usuario}.pythonanywhere.com/formulario")
+    return "ok"
+
+
 @app.route('/formulario', methods=['GET'])
 def get_form():
+    """rota para o formulário"""
     return render_template("formulario.html")
 
 
 @app.route('/trata-dados', methods=['POST'])
 def validate_form():
+    """rota que recebe os dados do formulário"""
     dados = request.form
     texto = f"Seu nome é {dados.get('name')}, seu endereço é {dados.get('address')} e você tem {dados.get('age')} anos :)"
+    try:
+        bot.sendMessage(dados.get('id_telegram'), texto)
+    except:
+        pass
+
     return texto
-
-
-app.run()
